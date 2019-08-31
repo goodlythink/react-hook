3f57a9153446e8356599e89cb45de00b82b8837c
goodlythink <goodlythink@gmail.com>
2019-08-31 08:23:18 +0700

    First Commit นะจ๊ะ

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
+const App = () => {
+  const [like, setLike] = useState(0)
+
+  useEffect(() => {
+    document.title = `This Like ${like}`
+  })
+
+  const increment = () => {
+    setLike(like + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>This is Hook</h2>
+      <h4>{like}</h4>
+      <button onClick={increment}>Like++</button>
     </div>
-  );
+  )
 }
+
+/*
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    like: 0
+  }
+
+  increment = () => {
+    this.setState({
+      like: this.state.like + 1
+    })
+  }
+
+  componentDidMount(){
+    document.title =`Clicked ${this.state.like}`
+  }
+
+  componentDidUpdate(){
+    document.title =`Clicked ${this.state.like}`
+  }
+
+  render() {
+    return (
+      <div>
+        Hello React
+      <hr />
+        <h4>{this.state.like}</h4>
+        <button onClick={this.increment}>Like++</button>
+      </div>
+    )
+  }
+}
+*/
 
 export default App;
