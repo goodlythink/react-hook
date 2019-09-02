6e3a987c3521a9cf037c5465b1efb00a29168904
goodlythink <goodlythink@gmail.com>
2019-09-02 14:53:57 +0700

    EP-3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import React, { useState, useEffect } from 'react'
 
 const App = () => {
-  const [like, setLike] = useState(0)
+  const [news, setNews] = useState([])
+  const [searchQuery, setSearchQuery] = useState('batman')
+  const [url, setUrl] = useState("http://hn.algolia.com/api/v1/search?query=batman")
+
+  const fetchNews = () => {
+    fetch(url)
+      .then(result => result.json())
+      // .then(data=>console.log(data.hits))
+      .then(data => setNews(data.hits))
+      .catch(error => console.log(error))
+  }
 
   useEffect(() => {
-    document.title = `This Like ${like}`
-  })
+    fetchNews()
+  }, [url])
 
-  const increment = () => {
-    setLike(like + 1)
+  const handleChange = e => {
+    setSearchQuery(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
   }
 
   return (
     <div>
-      <h2>This is Hook</h2>
-      <h4>{like}</h4>
-      <button onClick={increment}>Like++</button>
-    </div>
+      <h2>This is News</h2>
+      <form>
+        <input type="text" value={searchQuery} onChange={handleChange} />
+        <button onClick={handleSubmit}>Seach</button>
+      </form>
+
+      {news.map((n, i) => (<p key={i}>{n.title}</p>))}
+    </div >
   )
 }
 
